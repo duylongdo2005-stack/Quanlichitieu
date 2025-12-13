af10003fa8c999750f6f1764816ad3285d2dbda0
QuocBry <nguyenvanwuoc11112004@gmail.com>
2025-12-14 03:59:57 +0700

    Backend

--- Quan_Li_Chi_Tieu/LoginFrm.cs
+++ Quan_Li_Chi_Tieu/LoginFrm.cs
@@ -1,8 +1,8 @@
 namespace Quan_Li_Chi_Tieu
 {
-    public partial class Form1 : Form
+    public partial class LoginFrm : Form
     {
-        public Form1()
+        public LoginFrm()
         {
             InitializeComponent();
         }
